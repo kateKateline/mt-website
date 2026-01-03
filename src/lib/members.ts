1672ecf794ec46a412ce8bc9055956561d19fdaa
KateKateline <rizkyramadhan12098@gmail.com>
2026-01-03 15:57:34 +0700

    member

--- src/lib/members.ts
+++ src/lib/members.ts
@@ -1,12 +1,12 @@
 // Shared members data
-export const MEMBER_USERNAMES = ["KateKateline", "gaearon", "yyx990803", "sindresorhus", "tj", "addyosmani"];
+export const MEMBER_USERNAMES = ["KateKateline", "Rdethei", "RILL123", "LansDoank", "AnoUnz", "tj"];
 
 export const MEMBER_ROLES: Record<string, string> = {
-  KateKateline: "Creator of MT",
-  gaearon: "React Core Team",
-  yyx990803: "Creator of Vue.js",
-  sindresorhus: "Open Source Maintainer of Astro",
-  tj: "Creator of Express.js",
-  addyosmani: "Engineering Manager of Angular",
+  KateKateline: "Creator of Mt",
+  Rdethei: "Orang Ganteng",
+  RILL123: "Raja Iblis",
+  LansDoank: "Admin of Mt",
+  AnoUnz: "Contributor",
+  tj: "replacement member",
 };
 
